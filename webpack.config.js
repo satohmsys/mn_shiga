a3f5150a071462de685c63c340454f9cab997f04
Depart055 - satohm <satohmsys@gmail.com>
2018-03-13 19:01:50 +0900

    webpack.config Scssコンパイルまで完了

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,16 @@
 
 weback config
 for static website
+
+update : 201803
+
+-webpack4 x sass
+@link: https://ics.media/entry/17376
+@link: https://gist.github.com/mburakerman/629783c16acf5e5f03de60528d3139af
+
+-autoprefixer
+@link: https://blog.funxion.jp/314/
+
 -webpack
 @link: http://dackdive.hateblo.jp/entry/2016/05/07/183335
 @link: http://dackdive.hateblo.jp/entry/2016/04/13/123000
@@ -13,79 +23,116 @@ for static website
 
 const webpack = require( 'webpack' );
 const path = require( 'path' );
-const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+const MODE = 'development';
+const enabledSourceMap = ( MODE === 'development' );
+
+
 
 module.exports = [{
-	//developmentだとsource mapも有効化
-	mode: 'development',
+	/**
+	* mode
+	* v4 / developmentだとsource mapも有効化
+	*/
+	mode: MODE,
 
-	//エントリーポイントを指定しなければ自動的に「src/index.js」がエントリーポイントに
-	entry: './src/index.js',
+	devServer: {
+		contentBase: 'dist', //ここをルートとしてサーブ
+		hot: true,
+		inline: true,
+		open: true,
+		port: 3000,
+	},	
 
-	//
-	output: {
-		path: path.resolve( __dirname ),
-		// publicPath: 'dist',
-		filename: 'main.js'
+	/**
+	* entry point
+	* v4 / エントリーポイントを指定しなければ自動的に「src/index.js」がエントリーポイントに
+	*/
+	entry: [
+		path.resolve(__dirname, './src/index.js'),
+		path.resolve(__dirname, './src/scss/style.scss'),
+		// path.resolve(__dirname, './dist/index.html')
+	],
 
-	},
 
 	/**
 	*ファイル変換
 	*loaderは右から左に適用
 	* v3 module.loaders -> v4 module.rules
-	* loader -> css-loader!sass-loader
-	* use -> use: { 'css-loader','sass-loader'} 
+	* v3 / loader -> css-loader!sass-loader
+	* v4 / use -> use: [ 'css-loader',{loader:'sass-loader', options:{ ~~ }}] 
 	*/	
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
-				exclude: '/node_modules',
-				use: 'babel-loader',
-	            options: {
-	              presets: [
-	                // env を指定することで、ES2017 を ES5 に変換。
-	                // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
-	                // webpack の Tree Shaking 機能が使えない
-	                ['env', {'modules': false}]
-	              ]
-	            }
+				exclude: /node_modules/,
+				use: [{
+						loader: 'babel-loader',
+			            options: {
+			              presets: [
+			                // env を指定することで、ES2017 を ES5 に変換。
+			                // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
+			                // webpack の Tree Shaking 機能が使えない
+			                ['env', {'modules': false}]
+			              ]}
+			          }]
 			},
 			{
 				test: /\.scss$/,
-				exclude: '/node_modules',
-				use: ExtractTextPlugin.extract({
+				exclude: /node_modules/,
+				use: extractPlugin.extract({
 					fallback: 'style-loader',
 					use: [
-						'css-loader',
-						'sass-loader'
+						{
+							loader: 'css-loader',
+							options: {
+								url: false,
+								sourceMap: enabledSourceMap,
+								importLoaders: 2,
+
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: ( loader ) => [ require('autoprefixer') ],
+								sourceMap: enabledSourceMap								
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options:{
+								sourceMap: enabledSourceMap,
+								minimize: true								
+							}
+						}
 					]
 				})
 			},
 			{
-				test: /\.css$/,
-				exclude: '/node_modules',
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: 'css-loader'
-				})
-			},
-			{
 				test: /\.(gif|png|jpg|eot|woff|ttf|svg|css)$/,
-				exclude: '/node_modules',
+				exclude: /node_module/,
 				use: 'url-loader'
 			}
 		]
 	},
 
-	devServer: {
-		contentBase: 'dist',
-		open: true,
-		inline: true,
-		hot: true,
-		port: 3000		
+	/**
+	*
+	*/
+	output: {
+		path: path.resolve( __dirname, 'dist' ),
+		publicPath: '/dist',
+		filename: 'main.js'
 	},
+
+    plugins: [
+        extractPlugin
+    ],
 
 	resolve: {
 		extensions: ['.js', '.jsx'],

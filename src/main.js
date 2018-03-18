d957022e5025924e2c0a2d09cadaf0298d6dff2d
佐藤正康 <satohmsys@gmail.com>
2018-03-18 11:30:49 +0900

    webpack エントリーポイント名称変更

---
+++ src/main.js
@@ -0,0 +1,23 @@
+/*************************
+
+scripts
+
+*************************/
+
+/**
+* html監視設定
+*/
+import './html/index.html'; 
+/**
+* scss監視設定
+*/
+import './scss/style.scss';
+
+
+import $ from 'jquery';
+
+import common from './js/common.js';
+import top from './js/top.js';
+
+top()
+common();

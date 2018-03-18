36e6bf3e6e34b2d30582b51a430840f695c66dc7
佐藤正康 <satohmsys@gmail.com>
2018-03-18 14:46:29 +0900

    slickjs導入

--- src/js/top.js
+++ src/js/top.js
@@ -5,19 +5,51 @@ import { getScrollVal, $w } from './common.js';
 
 
 var effefcts = () => {
-	let $navPanels = $( '.section-navPanel' ).find( '.navPanel__panel' );
+	let $navPanels = $( '.section-navPanel' ).find( '.navPanel__panel' ),
+		$mainvisualArea = $('.mainvisual');
+	
 	let f = function( $scrollVal ){
+		const $scrollBottom = $scrollVal + $w.height();
+
+		/**
+		* panel navigation area
+		*/
 		$.each( $navPanels, function(){
 			let $target = $( this );
 
-			if( $target.offset().top < $scrollVal + $w.height() - 20 ){
+			if( $target.offset().top < $scrollBottom - 20 ){
 				$target.addClass( 'fadeIn' );
 			}
-		})
+		});
+
+		/**
+		* mainvisual 40th num
+		*/
+
+		if( $mainvisualArea.offset().top < $scrollBottom ) {
+			$mainvisualArea.addClass( 'fadeIn' );
+		}
+
+
 	};
 
 	getScrollVal( f ) ;
 
+
+	/**
+	* slick
+	*/
+	$( '.mainvisual__images' ).slick({
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 0,
+		dots: false,
+		fade: true,
+		infinite: true,
+		slideToShow: 1,
+		slideToScroll: 1,
+		speed: 8000
+	});
 }
 
 export default function(){

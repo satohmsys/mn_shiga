d4f299474d4ba33ae5a4bf5c0ea39475d52e318b
佐藤正康 <satohmsys@gmail.com>
2018-03-20 15:59:15 +0900

    about修正

--- src/js/page.js
+++ src/js/page.js
@@ -16,15 +16,15 @@ var effefcts = () => {
 			/**
 			* section culture
 			*/
-			if( $sectionCulture.offset().top < $scrollBottom  - 20  &&
-				 $scrollVal < ($sectionCulture.outerHeight() + $sectionCulture.offset().top ) ) {
-				let bgpY = 100 - $scrollVal *0.05;
+			// if( $sectionCulture.offset().top < $scrollBottom  - 20  &&
+			// 	 $scrollVal < ($sectionCulture.outerHeight() + $sectionCulture.offset().top ) ) {
+			// 	let bgpY = 100 - $scrollVal *0.05;
 
-				$icon40th.css({
-					'filter': 'blur(' + bgpY*2 + 'px)',
-					'transform': 'translateY(' + bgpY + '%)'
-				})
-			}			
+			// 	$icon40th.css({
+			// 		'filter': 'blur(' + bgpY*2 + 'px)',
+			// 		'transform': 'translateY(' + bgpY + '%)'
+			// 	})
+			// }			
 		};
 
 		getScrollVal( f ) ;
